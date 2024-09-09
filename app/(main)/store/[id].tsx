d8454f9987014d1b4ae38d37744927b2f783200c
jjfwang <jjfwang@outlook.coom>
2024-09-09 19:43:55 +0800

    store detail page

--- app/(main)/store/[id].tsx
+++ app/(main)/store/[id].tsx
@@ -1,7 +1,8 @@
 import { useGetStoreByIdQuery } from '@/services/store.service';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import { ActivityIndicator, Button, Card, Text } from 'react-native-paper';
 
 const Store = () => {
   const { id } = useLocalSearchParams();
@@ -9,13 +10,92 @@ const Store = () => {
     storeId: id,
   });
   return (
-    <View>
-      <Stack.Screen options={{ title: 'Store' }} />
-      <Text> Individual Store</Text>
-      <Text> This is the id: {id} get from previous screen.</Text>
-      <Text> Detail screen to be implemented. </Text>
-    </View>
+    <>
+      <Stack.Screen options={{ title: data?.data?.storeName }} />
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <ScrollView contentContainerStyle={styles.scrollViewStyle}>
+          <Card style={styles.container}>
+            <Card.Cover style={styles.cover} source={{ uri: data?.data?.image }} />
+            <Card.Title
+              title={data?.data?.description}
+              right={() => (
+                <Card.Actions>
+                  <Button mode="contained" onPress={() => {}}>
+                    Edit
+                  </Button>
+                </Card.Actions>
+              )}
+            />
+            <Card.Content>
+              <Text>Address</Text>
+              <Text style={styles.text}>
+                {data?.data?.address}, {data?.data?.address1}, {data?.data?.address2},
+                {data?.data?.address3}
+              </Text>
+              <Text>City</Text>
+              <Text style={styles.text}>{data?.data?.city}</Text>
+              <Text>State</Text>
+              <Text style={styles.text}>{data?.data?.state}</Text>
+              <Text>Country</Text>
+              <Text style={styles.text}>{data?.data?.country}</Text>
+              <Text>Postal Code</Text>
+              <Text style={styles.text}>{data?.data?.postalCode}</Text>
+              <Text>Contact</Text>
+              <Text style={styles.text}>{data?.data?.contactNumber}</Text>
+            </Card.Content>
+          </Card>
+        </ScrollView>
+      )}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+  },
+  scrollViewStyle: {
+    flex: 1,
+    padding: 15,
+  },
+  headingStyle: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  icon: {
+    alignItems: 'center',
+    margin: 10,
+    padding: 10,
+  },
+  field: {
+    marginBottom: 20,
+  },
+  container: {
+    alignContent: 'center',
+    margin: 10,
+  },
+  cover: {
+    margin: 10,
+  },
+  processbar: {
+    marginBottom: 10,
+  },
+  button: {
+    marginBottom: 10,
+  },
+  text: {
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    height: 45,
+    borderRadius: 5,
+    borderColor: 'grey',
+    backgroundColor: 'white',
+  },
+});
 
 export default Store;
