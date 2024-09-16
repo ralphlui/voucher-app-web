35a77295f66149c414ad7978da82acf197794de6
jjfwang <jjfwang@outlook.coom>
2024-09-16 23:37:32 +0800

    change EXPO_PUBLIC_AUTH_API_URL

--- services/user.api.ts
+++ services/user.api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const userApi = createApi({
   reducerPath: 'user',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.EXPO_PUBLIC_USER_API_URL,
+    baseUrl: process.env.EXPO_PUBLIC_AUTH_API_URL,
     headers: {
       'Content-Type': 'application/json',
     },
