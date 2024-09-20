3bb1ccd8454f956312db1fc827d244a97f5811f8
jjfwang <jjfwang@outlook.coom>
2024-09-20 13:58:16 +0800

    Map feed service

--- services/feed.service.ts
+++ services/feed.service.ts
@@ -3,11 +3,10 @@ import feedApi from '@/services/feed.api';
 export const feedApiSlice = feedApi.injectEndpoints({
   endpoints: (builder) => ({
     getFeedByEmail: builder.query({
-      query: ({ email, page_size = 5, page_number = 0 }) => ({
-        url: `/api/feed/getAllByEmail?page=${page_number}&size=${page_size}`,
-        method: 'POST', // to be changed to 'GET',
-        params: { page_size, page_number },
-        body: JSON.stringify({ email }),
+      query: ({ userId, page_size = 5, page_number = 0 }) => ({
+        url: `/api/feeds/users/${userId}?page=${page_number}&size=${page_size}`,
+        method: 'GET',
+        params: { userId, page_size, page_number },
       }),
       providesTags: ['Feed'],
       serializeQueryArgs: ({ endpointName }) => {
@@ -20,36 +19,26 @@ export const feedApiSlice = feedApi.injectEndpoints({
         return currentArg !== previousArg;
       },
     }),
+    getFeedById: builder.query({
+      query: (id: string) => ({
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        url: `/api/feeds/${id}`,
+        method: 'GET',
+      }),
+    }),
+    updateReadStatus: builder.mutation({
+      query: (id: string) => ({
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        url: `/api/feeds/${id}/readStatus`,
+        method: 'PATCH',
+      }),
+    }),
   }),
 });
 
-export const { useGetFeedByEmailQuery } = feedApiSlice;
-
-// ---- ---- ---- ---- ----legacy---- ---- ---- ---- ---- ----
-
-export const fetchAllActiveFeedListByCustomer = async (
-  email: string,
-  page?: number,
-  size?: number
-) => {
-  const requestBody = {
-    email,
-  };
-
-  let url = '';
-  if (page !== undefined) {
-    url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/feed/getAllByEmail?page=${page}&size=${size}`;
-  } else {
-    url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/feed/getAllByEmail`;
-  }
-  const res = await fetch(`${url}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestBody),
-  });
-
-  const data = await res.json();
-  return data;
-};
+export const { useGetFeedByEmailQuery, useGetFeedByIdQuery, useUpdateReadStatusMutation } =
+  feedApiSlice;
