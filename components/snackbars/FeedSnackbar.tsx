113b0b6bdb70bcc9dbb742700573938d8e174fca
jjfwang <jjfwang@outlook.coom>
2024-10-05 22:43:09 +0800

    fix snackbar styling for live feed

--- components/snackbars/FeedSnackbar.tsx
+++ components/snackbars/FeedSnackbar.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
 import { Snackbar, Text } from 'react-native-paper';
 import { useRouter } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
 
 const FeedSnackbar = () => {
   const message = useSelector((state: RootState) => state.auth.message);
@@ -14,19 +15,32 @@ const FeedSnackbar = () => {
     }
   }, [message]);
   return (
-    <Snackbar
-      visible={visible}
-      onDismiss={() => setVisible(false)}
-      duration={10000}
-      action={{
-        label: 'View',
-        onPress: () => {
-          router.push(`/(main)/campaign/${message?.campaignId ?? ''}`);
-        },
-      }}>
-      {message?.campaignDescription} @ {message?.storeName}
-    </Snackbar>
+    <View style={styles.container}>
+      <Snackbar
+        visible={visible}
+        onDismiss={() => setVisible(false)}
+        duration={10000}
+        action={{
+          label: 'View More',
+          onPress: () => {
+            router.push(`/(main)/campaign/${message?.campaignId ?? ''}`);
+          },
+        }}>
+        <Text style={styles.text}>
+          New promotion {message?.campaignDescription} @ {message?.storeName}
+        </Text>
+      </Snackbar>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+  },
+  text: {
+    color: 'white',
+  },
+});
 
 export default FeedSnackbar;
